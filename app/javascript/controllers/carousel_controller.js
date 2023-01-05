f372c387d754ee5239a556edcb51489628c382a3
Alex Drew <alexander.william.drew@gmail.com>
2023-01-05 18:43:53 +0000

    conected chevrons to stimulus controller functions

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -5,10 +5,13 @@ export default class extends Controller {
   connect() {
     console.log("Is this connected?");
   }
-  static targets = ["one", "two", "three"];
 
-  one() {
-    this.oneTarget.classList.add("hide-carousel")
+  left() {
+    console.log("left");
+  }
+
+  right() {
+    console.log("right");
   }
 
 }
