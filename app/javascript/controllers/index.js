9ca8f026fb6b8ea174c8c31131036ba204171815
Alex Drew <alexander.william.drew@gmail.com>
2023-01-19 16:44:52 +0800

    added animation to hourglass on scroll

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,17 +4,20 @@
 
 import { application } from "./application"
 
+import CarouselController from "./carousel_controller"
+application.register("carousel", CarouselController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import IconController from "./icon_controller"
+application.register("icon", IconController)
 
 import NavbarController from "./navbar_controller"
 application.register("navbar", NavbarController)
 
-import TypedJsController from "./typed_js_controller"
-application.register("typed-js", TypedJsController)
-
-import CarouselController from "./carousel_controller"
-application.register("carousel", CarouselController)
-
 import NavlistController from "./navlist_controller"
 application.register("navlist", NavlistController)
+
+import TypedJsController from "./typed_js_controller"
+application.register("typed-js", TypedJsController)
