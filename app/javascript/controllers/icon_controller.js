f892c07e2e1f6fcc9fd83205a5ac77eb7de24c42
Alex Drew <alexander.william.drew@gmail.com>
2023-01-21 22:59:17 +0800

    Added animation to stamp icon

--- app/javascript/controllers/icon_controller.js
+++ app/javascript/controllers/icon_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar"
 export default class extends Controller {
 
-  static targets = [ "hourglass-top", "hourglass-bottom" ]
+  static targets = [ "hourglass-top", "hourglass-bottom", "main-stamp", "stamp-ink" ]
 
   connect() {
     console.log("icons");
@@ -26,6 +26,36 @@ export default class extends Controller {
       bottomPart.classList.add("bottom-hourglass-fill");
     }else{
       bottomPart.classList.remove("bottom-hourglass-fill");
+    }
+  }
+
+  iconAnimateStampMain() {
+
+    let mainPart = document.getElementById("main-stamp");
+    let rect =mainPart.getBoundingClientRect();
+    console.log(rect.top);
+    if (rect.top <= (500)) {
+      mainPart.classList.remove("stamp-up");
+      mainPart.classList.add("stamp-down");
+    } else{
+      mainPart.classList.remove("stamp-down");
+      mainPart.classList.add("stamp-up");
+    }
+    if (rect.top < (450)) {
+      mainPart.classList.add("stamp-up");
+      mainPart.classList.remove("stamp-down");
+    }
+
+  }
+
+  iconAnimateStampInk() {
+    let inkPart  = document.getElementById("ink");
+    let mainPart = document.getElementById("main-stamp");
+    let rect =mainPart.getBoundingClientRect();
+    if (rect.top <= (500)) {
+        // inkPart.classList.remove("ink-hidden");
+    } else{
+      inkPart.classList.add("ink-hidden");
     }
   }
 }
