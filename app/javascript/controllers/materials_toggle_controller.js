6265edb9b641a0c0ad1d95d01d35c11689996c7c
Alex Drew <alexander.william.drew@gmail.com>
2023-02-18 16:04:56 +0800

    JavaScript for materials headers added.

--- app/javascript/controllers/materials_toggle_controller.js
+++ app/javascript/controllers/materials_toggle_controller.js
@@ -14,6 +14,10 @@ export default class extends Controller {
     let graniteSection = this.element.querySelector(".active-materials-section");
     let quartzSection = this.element.querySelector(".materials-quartz-section");
     let ceramicSection = this.element.querySelector(".materials-ceramic-section");
+    let selectedHeader = this.element.querySelector(".selected-material");
+    let graniteHeader = this.element.querySelector(".granite-header");
+    selectedHeader.classList.remove("selected-material");
+    graniteHeader.classList.add("selected-material");
     graniteSection.classList.remove("inactive-materials-section");
     quartzSection.classList.add("inactive-materials-section");
     ceramicSection.classList.add("inactive-materials-section");
@@ -24,6 +28,10 @@ export default class extends Controller {
     let graniteSection = this.element.querySelector(".active-materials-section");
     let quartzSection = this.element.querySelector(".materials-quartz-section");
     let ceramicSection = this.element.querySelector(".materials-ceramic-section");
+    let selectedHeader = this.element.querySelector(".selected-material");
+    let quartzHeader = this.element.querySelector(".quartz-header");
+    selectedHeader.classList.remove("selected-material");
+    quartzHeader.classList.add("selected-material");
     quartzSection.classList.remove("inactive-materials-section");
     graniteSection.classList.add("inactive-materials-section");
     ceramicSection.classList.add("inactive-materials-section");
@@ -34,6 +42,10 @@ export default class extends Controller {
     let graniteSection = this.element.querySelector(".active-materials-section");
     let quartzSection = this.element.querySelector(".materials-quartz-section");
     let ceramicSection = this.element.querySelector(".materials-ceramic-section");
+    let selectedHeader = this.element.querySelector(".selected-material");
+    let ceramicHeader = this.element.querySelector(".ceramic-header");
+    selectedHeader.classList.remove("selected-material");
+    ceramicHeader.classList.add("selected-material");
     ceramicSection.classList.remove("inactive-materials-section");
     graniteSection.classList.add("inactive-materials-section");
     quartzSection.classList.add("inactive-materials-section");
