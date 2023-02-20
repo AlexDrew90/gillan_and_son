dc44fc43eae5e2706db15b6fa5804a90822a3ed8
Alex Drew <alexander.william.drew@gmail.com>
2023-02-20 22:55:04 +0800

    Fixed Navbar issue and updated incorrect fonts.

--- app/javascript/controllers/navlist_controller.js
+++ app/javascript/controllers/navlist_controller.js
@@ -3,22 +3,22 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar"
 export default class extends Controller {
 
-  static targets = [ "menu", "icon" ]
+
 
 
 connect() {
-    console.log("dropdown");
+
   }
 
   list() {
-    if (this.menuTarget.classList.contains("hide-nav-list")) {
-    this.menuTarget.classList.remove("hide-nav-list");
-    this.iconTarget.classList.add("move-icon");
+
+    let navLinks = this.element.querySelector(".navbar-links");
+    if (navLinks.classList.contains("hide-nav-list")){
+      navLinks.classList.remove("hide-nav-list");
     }
     else{
-      this.menuTarget.classList.add("hide-nav-list");
-      this.iconTarget.classList.remove("move-icon");
-      }
+      navLinks.classList.add("hide-nav-list");
+    }
   }
 
 
