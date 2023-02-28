5295225418e8c8677b1b75251a6dc800e914a468
Alex Drew <alexander.william.drew@gmail.com>
2023-02-28 20:53:16 +0800

    Fixed button CTA jump at different screen widths.

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -7,7 +7,8 @@ export default class extends Controller {
   }
   updateNavbar() {
     // if (window.scrollY >= (100 + this.element.offsetHeight)) {
-    if (window.scrollY >= (360)) {
+      const width = window.innerWidth;
+    if ((width >= 769 && window.scrollY >= (520)) || (width <= 768 && window.scrollY >= (360))) {
       this.element.classList.add("button-banner");
       this.element.classList.remove("button-div");
     } else {
