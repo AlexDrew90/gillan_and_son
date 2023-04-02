00ea4202f07f3ce0a4e0d63885e80976715abf20
Alex Drew <alexander.william.drew@gmail.com>
2023-04-02 11:31:30 +0800

    Added view for quote section and linked to it from relevant pages. Have also added a three column layout for the intro to the quote tool.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_03_151228) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_142752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_03_151228) do
     t.datetime "updated_at", null: false
     t.string "material_type"
     t.index ["brand_id"], name: "index_materials_on_brand_id"
+  end
+
+  create_table "requirements", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.string "description"
+    t.string "input"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
