f926fbcf8bc6685af88472825be88a532e32895a
Alex Drew <alexander.william.drew@gmail.com>
2023-04-11 22:43:58 +0800

    Question icon now generates an image preview of the requrement.

--- app/javascript/controllers/requirement_controller.js
+++ app/javascript/controllers/requirement_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="requirement"
 export default class extends Controller {
   connect() {
+    // Slideshow functionality
     let slideshowElements = document.getElementsByClassName("requirement-demo-slideshow");
     let currentSlideCounter = 0;
     slideshowElements[currentSlideCounter].classList.remove("hide-slide-image");
@@ -10,44 +11,51 @@ export default class extends Controller {
     function changeSlide() {
       slideshowElements[currentSlideCounter].classList.add("hide-slide-image");
       currentSlideCounter = (currentSlideCounter + 1) % arrayLength;
-      console.log(currentSlideCounter);
       slideshowElements[currentSlideCounter].classList.remove("hide-slide-image");
     }
 
     this.slideChangeInterval = setInterval(changeSlide, 4000);
+
+    // Attach event listener for expand method
+    this.element.addEventListener("click", this.expand.bind(this));
   }
+
 
   disconnect() {
     clearInterval(this.slideChangeInterval);
+    this.element.removeEventListener("click", this.expand.bind(this));
   }
 
-expand() {
-  window.onclick = e => {
-   let imageSource =  e.target.id
-   console.log(imageSource);
-   return imageSource
-};
-// console.log(imageSource);
-//   let imgModal = (imageSource) => {
-//     const modal = document.createElement("div");
-//     modal.setAttribute("class", "modal");
-//     //add the modal to the main section or the parent element
-//     document.querySelector(".quote-build-wrapper").append(modal);
-//     //adding image to modal
-//   const newImage = document.createElement("img");
-//   newImage.setAttribute("src", imageSource);
-//   modal.append(newImage)
-//   //adding the close button to modal
-//   closeBtn.setAttribute("class", "fas fa-times closeBtn");
+
+  expand(e) {
+    if (e.target.classList.contains("requirement-icons")) {
+      let imageSource = `/assets/requirements/${e.target.dataset.photoUrl}`;
+      const modal = document.createElement("div");
+      modal.setAttribute("class", "modal");
+      const closeBtn = document.createElement("i");
+      closeBtn.setAttribute("class", "fas fa-times closeBtn");
+
+      let imgModal = (imageSource, modal, closeBtn) => {
+        //add the modal to the main section or the parent element
+        document.querySelector(".requirements-intro-section").append(modal);
+        //adding image to modal
+        const newImage = document.createElement("img");
+        newImage.setAttribute("src", imageSource);
+        modal.append(newImage);
+        //adding the close button to modal
+        modal.append(newImage, closeBtn);
+
+        //close function
+        closeBtn.onclick = () => {
+          modal.remove();
+        };
+      };
+
+      imgModal(imageSource, modal, closeBtn);
+    }
+  }
 
 
-//   //close function
-//   closeBtn.onclick = () => {
-//   modal.remove();};
 
-// modal.append(newImage, closeBtn);
-// };
 
-// imgModal(imageSource);
-}
 }
