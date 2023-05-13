2807571605b86ecf0af3e3421552577fa0d5c839
Alex Drew <alexander.william.drew@gmail.com>
2023-05-13 14:17:52 +0800

    Working through Cloudinary issues.

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -12,4 +12,42 @@ class MaterialsController < ApplicationController
   def new
     @material = Material.new
   end
+
+  # def create
+  #   @material = Material.new(material_params)
+  #   @material.save
+  #   redirect_to cms_path
+  # end
+
+  def create
+    @material = Material.new(material_params)
+    if @material.save
+      redirect_to cms_path
+    else
+      flash.now[:alert] = "Error creating material."
+      render :new
+    end
+  end
+
+  def edit
+    @material = Material.find(params[:id])
+  end
+
+  def update
+    @material = Material.find(params[:id])
+    @material.update(material_params)
+    redirect_to material_path(@material)
+  end
+
+  def destroy
+    @material = Material.find(params[:id])
+    @material.destroy
+    redirect_to cms_path, status: :see_other
+  end
+
+  private
+
+  def material_params
+    params.require(:material).permit(:name, :brand_id, :photo, :material_type)
+  end
 end
